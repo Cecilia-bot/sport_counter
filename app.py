78676f2b7522001dd6c47f05b38dac8551ec7032
Cecilia Barile <97842174+Cecilia-bot@users.noreply.github.com>
2024-12-03 23:04:09 +0100

    SC-7: creae function to read user.txt file

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #backend using flask framework
 
 from flask import Flask, render_template, request, jsonify
+import glob
+import os
 
 app = Flask(__name__)
 
@@ -8,17 +10,34 @@ app = Flask(__name__)
 def get():
     return jsonify({'message': 'something'})
 
-@app.route('/api', methods=['POST'])
-def api():
-    if request.method == "POST":
-        user = request.form.get("users")
-        # test = request.form.get("test")
-        # return f"Received data: {user}-{test}"
-        return f"Received data: {user}"
+# @app.route('/api', methods=['POST'])
+# def api():
+#     if request.method == "POST":
+#         user = request.form.get("users")
+#         # test = request.form.get("test")
+#         # return f"Received data: {user}-{test}"
+#         return f"Received data: {user}"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def list_users():
+    users_path = glob.glob("assets/data/*.txt")
+    users = []
+    for user in users_path:
+        users.append(os.path.splitext(os.path.basename(user))[0])
+    return users
+
+@app.route('/api', methods=['POST'])
+def read_user(): # needs to be adapted for both user1 and 2!!!!
+    if request.method == "POST":
+        action = request.form.get("action")
+        user = request.form.get("users")
+        if action == "select_user":
+            with open("assets/data/" + user + ".txt") as user_file:
+                return user_file.read()
+
 if __name__ == "__main__":
     app.run(debug=True)
+    # print(list_users())
