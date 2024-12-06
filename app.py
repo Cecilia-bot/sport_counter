2451be78c9bbd8cb2de78cb6cda7544593a94c16
Cecilia Barile <97842174+Cecilia-bot@users.noreply.github.com>
2024-12-06 19:21:31 +0100

    SC-7: display the user count (.txt content) in html page

--- app.py
+++ app.py
@@ -18,25 +18,28 @@ def get():
 #         # return f"Received data: {user}-{test}"
 #         return f"Received data: {user}"
 
-@app.route('/')
+# def list_users():
+#     users_path = glob.glob("assets/data/*.txt")
+#     users = []
+#     for user in users_path:
+#         users.append(os.path.splitext(os.path.basename(user))[0])
+#     return users
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
-
-def list_users():
-    users_path = glob.glob("assets/data/*.txt")
-    users = []
-    for user in users_path:
-        users.append(os.path.splitext(os.path.basename(user))[0])
-    return users
-
-@app.route('/api', methods=['POST'])
-def read_user(): # needs to be adapted for both user1 and 2!!!!
+    count = 0
+    user = None
     if request.method == "POST":
-        action = request.form.get("action")
-        user = request.form.get("users")
-        if action == "select_user":
-            with open("assets/data/" + user + ".txt") as user_file:
-                return user_file.read()
+        count, user = read_user()
+        # counter=count --> counter is in html, count in python
+    return render_template('index.html', counter=count, user=user)
+
+def read_user(): # needs to be adapted for both user1 and 2!!!!
+    action = request.form.get("action")
+    user = request.form.get("users")
+    if action == "select_user":
+        with open("assets/data/" + user + ".txt") as user_file:
+            return user_file.read(), user
 
 if __name__ == "__main__":
     app.run(debug=True)
