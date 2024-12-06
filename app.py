8e3af1e5f8ed39e90af7c8ce8e40e45d594b0daf
Cecilia Barile <97842174+Cecilia-bot@users.noreply.github.com>
2024-12-06 21:41:16 +0100

    SC-7: make add_one button functional

--- app.py
+++ app.py
@@ -1,46 +1,34 @@
 #backend using flask framework
 
 from flask import Flask, render_template, request, jsonify
-import glob
-import os
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
-def get():
-    return jsonify({'message': 'something'})
-
-# @app.route('/api', methods=['POST'])
-# def api():
-#     if request.method == "POST":
-#         user = request.form.get("users")
-#         # test = request.form.get("test")
-#         # return f"Received data: {user}-{test}"
-#         return f"Received data: {user}"
-
-# def list_users():
-#     users_path = glob.glob("assets/data/*.txt")
-#     users = []
-#     for user in users_path:
-#         users.append(os.path.splitext(os.path.basename(user))[0])
-#     return users
-
+# route == function from flask (app)
+# route defines html path and methods that will call the python function (index)
 @app.route('/', methods=["GET", "POST"])
 def index():
     count = 0
     user = None
+    selected_user = request.form.get("users")
+    action = request.form.get("action")
     if request.method == "POST":
-        count, user = read_user()
+        if action == "select_user":
+            count, user = read_user(selected_user)
+        if action == "add":
+            current_count, user = read_user(selected_user)
+            count = add_one(current_count, user)
         # counter=count --> counter is in html, count in python
     return render_template('index.html', counter=count, user=user)
 
-def read_user(): # needs to be adapted for both user1 and 2!!!!
-    action = request.form.get("action")
-    user = request.form.get("users")
-    if action == "select_user":
-        with open("assets/data/" + user + ".txt") as user_file:
-            return user_file.read(), user
+def read_user(user): # needs to be adapted for both user1 and 2!!!!
+    with open("assets/data/" + user + ".txt") as user_file:
+        return user_file.read(), user
+
+def add_one(count, user):
+    with open("assets/data/" + user + ".txt", "w") as user_file:
+        user_file.write(str(int(count) + 1))
+        return int(count) + 1
 
 if __name__ == "__main__":
     app.run(debug=True)
-    # print(list_users())
