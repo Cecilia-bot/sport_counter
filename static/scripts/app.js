1237b0fa6f2e1da4ad9635449cdcb5cd76a38aeb
Cecilia Barile <97842174+Cecilia-bot@users.noreply.github.com>
2025-01-09 22:46:32 +0100

    SC-14: Feature: allow user creation

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
             }
         });
     });
-    $("button[id='add_user']").click(function (event) {
+    $("button[id='add']").click(function (event) {
         event.preventDefault(); // Prevent the page from refreshing
         var username = $("input[name='users']").val(); 
         if (!username) {
@@ -33,6 +33,28 @@ $(document).ready(function () {
             success: function (response) { 
                 // Update the HTML with the new data returned by the server
                 $("#counter").text(response.counter);
+            },
+            error: function () {
+                console.error("Error while sending request to the backend");
+            }
+        });
+    });
+    $("button[id='create_user']").click(function (event) {
+        event.preventDefault(); // Prevent the page from refreshing
+        var user = $("input[name='new_user']").val(); 
+        if (!user) {
+            alert("Please type a username before adding.");
+            return;
+        }
+        $.ajax({
+            url: "http://127.0.0.1:5000/",
+            type: "POST",
+            data: { new_user: user, action: "create_user" },
+            success: function (response) { 
+                alert("User " + response.new_user + " successfully created!");
+                $("input[name='new_user']").val("")
+                $("input[name='users']").val(response.new_user)
+                $("button[id='select_user']").click()
             },
             error: function () {
                 console.error("Error while sending request to the backend");
