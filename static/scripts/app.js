ae29b800b4c4384f17f85038e750ecb863f78312
Cecilia Barile <97842174+Cecilia-bot@users.noreply.github.com>
2025-01-16 22:28:48 +0100

    SC-20: Feature: create user when it does not exist

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -11,8 +11,29 @@ $(document).ready(function () {
             type: "POST",
             data: { users: username, action: "select_user" },
             success: function (response) {
-                // Update the HTML with the new data returned by the server
-                $("#counter").text(response.counter);
+                if (response.exists) {
+                    // Update the HTML with the new data returned by the server
+                    $("#current_user").text(response.user);
+                    $("#counter").text(response.counter);
+                } else {
+                    const createFile = confirm("User " + username + " does not exist. Would you like to create it?");
+                    if (createFile) {
+                        $.ajax({
+                            url: "http://127.0.0.1:5000/",
+                            type: "POST",
+                            data: { users: username, action: "create_user" },
+                            success: function (response) { 
+                                alert("User " + response.user + " successfully created!");
+                                $("input[name='users']").val(response.user)
+                                $("button[id='select_user']").click()
+                            },
+                            error: function () {
+                                console.error("Error while sending request to the backend");
+                            }
+                        })
+                    }
+                }
+                
             },
             error: function () {
                 console.error("Error while sending request to the backend");
@@ -33,28 +54,6 @@ $(document).ready(function () {
             success: function (response) { 
                 // Update the HTML with the new data returned by the server
                 $("#counter").text(response.counter);
-            },
-            error: function () {
-                console.error("Error while sending request to the backend");
-            }
-        });
-    });
-    $("button[id='create_user']").click(function (event) {
-        event.preventDefault(); // Prevent the page from refreshing
-        var user = $("input[name='new_user']").val(); 
-        if (!user) {
-            alert("Please type a username before adding.");
-            return;
-        }
-        $.ajax({
-            url: "http://127.0.0.1:5000/",
-            type: "POST",
-            data: { new_user: user, action: "create_user" },
-            success: function (response) { 
-                alert("User " + response.new_user + " successfully created!");
-                $("input[name='new_user']").val("")
-                $("input[name='users']").val(response.new_user)
-                $("button[id='select_user']").click()
             },
             error: function () {
                 console.error("Error while sending request to the backend");
