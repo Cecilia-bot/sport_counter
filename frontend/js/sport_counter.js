c324769dfb4037a0fad438f7c070b0929b976cca
Cecilia Barile <97842174+Cecilia-bot@users.noreply.github.com>
2026-02-08 17:50:37 +0100

    SC-72: API adapted

--- frontend/js/sport_counter.js
+++ frontend/js/sport_counter.js
@@ -1,5 +1,5 @@
-const API_BASE = "http://127.0.0.1:8000";
-// const API_BASE = "https://sportcounter-backend.up.railway.app";
+// const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://sportcounter-backend.up.railway.app";
 
 const resortSelect = document.getElementById('resort');
 const addBtn = document.getElementById("addBtn");
--- frontend/js/visits.js
+++ frontend/js/visits.js
@@ -1,5 +1,5 @@
-const API_BASE = "http://127.0.0.1:8000";
-//const API_BASE = "https://sportcounter-backend.up.railway.app";
+// const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://sportcounter-backend.up.railway.app";
 
 async function waitForAuth() {
     return new Promise(resolve => {
